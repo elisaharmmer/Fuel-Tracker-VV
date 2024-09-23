515fa84d5a3eb490dd4c3e9f670644e5edd82dc3
Nícolas Aigner <nicolas.aigner@gmail.com>
2024-09-23 16:58:47 -0300

    Ajustes para realizar o export para csv, excel e pdf da lista de postos

--- frontend-angular/src/app/components/gas-station-list/gas-station-list.component.ts
+++ frontend-angular/src/app/components/gas-station-list/gas-station-list.component.ts
@@ -1,36 +1,56 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { GasStationService } from "../../services/gas-station.service";
-import {PostoCombustivelDetalhado} from "../../models/posto";
-import {Combustivel} from "../../models/combustivel";
+import { PostoCombustivelDetalhado } from "../../models/posto";
+import { Combustivel } from "../../models/combustivel";
 import * as XLSX from 'xlsx';
 import { saveAs } from 'file-saver';
 import jsPDF from 'jspdf';
 import 'jspdf-autotable';
-
+import { Subscription } from "rxjs";
+import { DownloadEventService } from "../../services/download-event.service";
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-gas-station-list',
   templateUrl: './gas-station-list.component.html',
   styleUrls: ['./gas-station-list.component.scss']
 })
-export class GasStationListComponent implements OnInit {
+export class GasStationListComponent implements OnInit, OnDestroy {
   gasStations: PostoCombustivelDetalhado[] = [];
   neighborhoods: string[] = [];
   fuelTypes: string[] = [];
-
   searchTerm = '';
   selectedNeighborhood = '';
   selectedFuelType = '';
-
   filteredOptions: string[] = [];
   filteredGasStations: PostoCombustivelDetalhado[] = [];
+  private downloadSubscription: Subscription = new Subscription();
 
-  constructor(private gasStationService: GasStationService) {}
+  constructor(
+    private gasStationService: GasStationService,
+    private downloadEventService: DownloadEventService
+  ) {}
 
   ngOnInit(): void {
+    this.downloadSubscription = this.downloadEventService.downloadEvent$.subscribe(type => {
+      if (type === 'pdf') {
+        this.exportToPDF();
+      } else if (type === 'csv') {
+        this.exportToCSV();
+      } else if (type === 'excel') {
+        this.exportToExcel();
+      }
+    });
+
     this.loadGasStations();
     this.loadCombustiveis();
     this.loadBairros();
+  }
+
+  ngOnDestroy(): void {
+    if (this.downloadSubscription) {
+      this.downloadSubscription.unsubscribe();
+    }
   }
 
   loadGasStations() {
@@ -40,7 +60,7 @@ export class GasStationListComponent implements OnInit {
         this.filteredGasStations = this.gasStations;
         this.filteredOptions = this.gasStations.map(station => station.posto_nome);
       },
-      (error) => {
+      error => {
         console.error('Erro ao buscar os postos', error);
       }
     );
@@ -51,7 +71,7 @@ export class GasStationListComponent implements OnInit {
       (data: Combustivel[]) => {
         this.fuelTypes = data.map(combustivel => combustivel.nome);
       },
-      (error) => {
+      error => {
         console.error('Erro ao buscar os combustíveis', error);
       }
     );
@@ -62,7 +82,7 @@ export class GasStationListComponent implements OnInit {
       (data: { bairro: string }[]) => {
         this.neighborhoods = data.map(b => b.bairro);
       },
-      (error) => {
+      error => {
         console.error('Erro ao buscar os bairros', error);
       }
     );
@@ -72,7 +92,7 @@ export class GasStationListComponent implements OnInit {
     this.searchTerm = '';
     this.selectedNeighborhood = '';
     this.selectedFuelType = '';
-    this.filterStations(); // Atualiza a lista com todos os postos
+    this.filterStations();
   }
 
   clearFilter(filterType: string) {
@@ -82,7 +102,7 @@ export class GasStationListComponent implements OnInit {
         break;
       case 'neighborhood':
         this.selectedNeighborhood = '';
-        this.searchTerm = ''; // Limpa também o autocomplete
+        this.searchTerm = '';
         break;
       case 'fuelType':
         this.selectedFuelType = '';
@@ -91,59 +111,113 @@ export class GasStationListComponent implements OnInit {
     this.filterStations();
   }
 
-
   filterStations() {
-    // Verifica se o bairro foi alterado e limpa o campo de busca
     if (this.selectedNeighborhood) {
-      this.searchTerm = ''; // Limpa o valor selecionado no autocomplete
+      this.searchTerm = '';
     }
 
     this.filteredGasStations = this.gasStations.filter(station => {
       const matchesSearch = station.posto_nome.toLowerCase().includes(this.searchTerm.toLowerCase());
       const matchesNeighborhood = !this.selectedNeighborhood || station.posto_bairro === this.selectedNeighborhood;
-      const matchesFuelType = !this.selectedFuelType || station.combustiveis.some(combustivel => combustivel.combustivel_nome.toLowerCase().includes(this.selectedFuelType.toLowerCase()));
+      const matchesFuelType = !this.selectedFuelType || station.combustiveis.some(combustivel =>
+        combustivel.combustivel_nome.toLowerCase().includes(this.selectedFuelType.toLowerCase())
+      );
       return matchesSearch && matchesNeighborhood && matchesFuelType;
     });
 
-    // Atualiza as opções do autocomplete para mostrar apenas os postos do bairro selecionado
     this.filteredOptions = this.filteredGasStations.map(station => station.posto_nome);
   }
 
   exportToExcel(): void {
-    const dataToExport = this.filteredGasStations; // Substitua 'this.data' com a variável que contém os dados que deseja exportar
+    const dataToExport = this.getDataForExport();
     const worksheet: XLSX.WorkSheet = XLSX.utils.json_to_sheet(dataToExport);
-    const workbook: XLSX.WorkBook = {
-      Sheets: { 'data': worksheet },
-      SheetNames: ['data']
-    };
+    const workbook: XLSX.WorkBook = { Sheets: { data: worksheet }, SheetNames: ['data'] };
     const excelBuffer: any = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' });
     this.saveAsExcelFile(excelBuffer, 'dados_exportados');
   }
 
   private saveAsExcelFile(buffer: any, fileName: string): void {
-    const data: Blob = new Blob([buffer], {
-      type: 'application/octet-stream'
-    });
+    const data: Blob = new Blob([buffer], { type: 'application/octet-stream' });
     saveAs(data, `${fileName}_${new Date().getTime()}.xlsx`);
   }
 
   exportToPDF(): void {
     const doc = new jsPDF();
+    const colunasPrincipais = ['Posto', 'Bairro'];
+    let currentY = 10; // Posição inicial no eixo Y
 
-    // Colunas e linhas baseadas na sua fonte de dados
-    const colunas = Object.keys(this.filteredGasStations[0]);
-    const linhas = this.filteredGasStations.map((row: any) => colunas.map(col => row[col]));
+    this.filteredGasStations.forEach(station => {
+      const linhasPrincipais = [
+        [station.posto_nome, station.posto_bairro]
+      ];
 
-    // AutoTabela para adicionar os dados no PDF
-    (doc as any).autoTable({
-      head: [colunas],
-      body: linhas
+      // Tabela principal para Posto e Bairro
+      (doc as any).autoTable({
+        head: [colunasPrincipais],
+        body: linhasPrincipais,
+        startY: currentY
+      });
+
+      // Atualiza currentY para a posição após a tabela principal
+      currentY = (doc as any).lastAutoTable.finalY + 5;
+
+      // Subtabela para Combustíveis
+      const colunasCombustivel = ['Combustível', 'Data de Coleta', 'Valor'];
+      const linhasCombustivel = station.combustiveis.map(combustivel => [
+        combustivel.combustivel_nome,
+        combustivel.data_coleta ? moment(combustivel.data_coleta).format('DD/MM/YYYY') : 'N/A',
+        combustivel.preco ? `R$ ${combustivel.preco.replace('.', ',')}` : 'N/A'
+      ]);
+
+      // Inserindo a subtabela com os combustíveis
+      (doc as any).autoTable({
+        head: [colunasCombustivel],
+        body: linhasCombustivel,
+        startY: currentY,
+        theme: 'grid'
+      });
+
+      // Atualiza currentY para a posição após a subtabela
+      currentY = (doc as any).lastAutoTable.finalY + 10;
     });
 
-    // Salvar o PDF com um nome único
-    doc.save(`dados_exportados_${new Date().getTime()}.pdf`);
+    // Salvar o PDF
+    doc.save(`dados_exportados_${moment().format('DD-MM-YYYY-h-mm-ss-ms')}.pdf`);
   }
 
+  getDataForExport(): any[] {
+    const dataToExport: any[] = [];
 
+    this.filteredGasStations.forEach(station => {
+      station.combustiveis.forEach(combustivel => {
+        dataToExport.push({
+          Posto: station.posto_nome,
+          Bairro: station.posto_bairro,
+          Combustível: combustivel.combustivel_nome,
+          'Data de Coleta': combustivel.data_coleta
+            ? moment(combustivel.data_coleta).format('DD/MM/YYYY')
+            : 'N/A',
+          Valor: combustivel.preco
+            ? `R$ ${combustivel.preco.replace('.', ',')}`
+            : 'N/A'
+        });
+      });
+    });
+
+    return dataToExport;
+  }
+
+  convertToCSV(data: any[]): string {
+    const header = Object.keys(data[0]).join(';') + '\n'; // Usa ponto e vírgula como separador
+    const rows = data.map(row => Object.values(row).join(';')).join('\n');
+    return header + rows;
+  }
+
+  exportToCSV(): void {
+    const dataToExport = this.getDataForExport();
+    const csvData = this.convertToCSV(dataToExport);
+    const blob = new Blob([csvData], { type: 'text/csv;charset=utf-8;' });
+    saveAs(blob, `dados_exportados_${new Date().getTime()}.csv`);
+  }
 
 }
